524f418106676267381e33ad80d87a4a14878799
Aarav Jain <137067931+Aarav-Jain123@users.noreply.github.com>
2026-05-18 21:47:23 +0530

    Update api.ts

--- api.ts
+++ api.ts
@@ -1,2 +1,2 @@
-export const baseURL = import.meta.env.BASE_URL ||  process.env.BASE_URL
-export const geminiKEY = import.meta.env.GEMINI_API_KEY || process.env.GEMINI_API_KEY
+export const baseURL = process.env.BASE_URL
+export const geminiKEY = process.env.GEMINI_API_KEY
